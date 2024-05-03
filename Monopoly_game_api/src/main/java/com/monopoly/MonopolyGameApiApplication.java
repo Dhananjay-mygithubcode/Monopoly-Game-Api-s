f172a91bf26e0b6250e0834127b83ea098eb5abf
dhananjay <dhananjay2099@gmail.com>
2024-05-03 09:49:51 +0530

    monopoly

--- Monopoly_game_api/src/main/java/com/monopoly/MonopolyGameApiApplication.java
+++ Monopoly_game_api/src/main/java/com/monopoly/MonopolyGameApiApplication.java
@@ -1,13 +1,27 @@
 package com.monopoly;
 
+import java.util.Random;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MonopolyGameApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MonopolyGameApiApplication.class, args);
+	}
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+	
+	@Bean
+	public Random random(){ 
+		return new Random();
 	}
 	
 
