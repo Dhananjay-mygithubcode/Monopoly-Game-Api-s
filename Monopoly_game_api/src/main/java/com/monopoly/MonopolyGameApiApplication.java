7a14119a7cfb79eb463ee97ffd0a12b392438761
dhananjay <dhananjay2099@gmail.com>
2024-05-03 08:05:17 +0530

    monopoly-game

---
+++ Monopoly_game_api/src/main/java/com/monopoly/MonopolyGameApiApplication.java
@@ -0,0 +1,14 @@
+package com.monopoly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonopolyGameApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonopolyGameApiApplication.class, args);
+	}
+	
+
+}
